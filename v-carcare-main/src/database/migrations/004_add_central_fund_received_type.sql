9f71fcf4f849697b05145a9cf350a70e0fe7a148
6610122115063-tech <6610122115063@pnru.ac.th>
2026-09-08 16:52:57 +0000

    Update code

--- v-carcare-main/src/database/migrations/004_add_central_fund_received_type.sql
+++
@@ -1,8 +0,0 @@
--- Required only when migration 003 was applied before the fund-receipt action existed.
-ALTER TABLE central_fund_transactions
-    DROP CONSTRAINT IF EXISTS central_fund_transactions_movement_type_check;
-
-ALTER TABLE central_fund_transactions
-    ADD CONSTRAINT central_fund_transactions_movement_type_check
-    CHECK (movement_type IN
-        ('income', 'expense', 'opening_float', 'closing_float', 'adjustment', 'fund_received'));
